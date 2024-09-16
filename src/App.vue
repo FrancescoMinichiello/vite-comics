ff4e4d8c5c86e068feaea5df0695b49a3bc85843
FrancescoMinichiello <fraps2278@gmail.com>
2024-09-16 17:57:43 +0200

    Added bg main

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,7 @@ export default{
   <!--Header-->
   <AppHeader/>
   <AppMain/>
+  <AppFooter/>
 </template>
 
 <style lang="scss" scoped>
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,9 +9,31 @@ export default{
 </script>
 
 <template>
+  <footer>
+    <div class="container-fluid">
+      <div class="row row-cols-2">
+        <div class="col footer-links">
 
+        </div>
+        <div class="col footer-image">
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss" scoped>
 @use 'bootstrap/scss/bootstrap.scss' as *;
+footer{
+  min-height: 500px;
+  background-image: url("../assets/img/footer-bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  .footer-image{
+    background-image: url("../assets/img/dc-logo-bg.png");
+    background-repeat: no-repeat;
+    min-height: 500px;
+  }
+}
+
 </style>    
