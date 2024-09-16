57ffe8b320a278caee756322c61af532a10f1781
FrancescoMinichiello <fraps2278@gmail.com>
2024-09-16 16:59:27 +0200

    Added AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,18 @@
 export default{
   data(){
     return{
-
+        navLinks:[
+            "Characters",
+            "Comics",
+            "Movies",
+            "Tv",
+            "Games",
+            "Collectibles",
+            "Videos",
+            "Fans",
+            "News",
+            "Shop",
+        ]
     }
   }
 }
@@ -10,13 +21,22 @@ export default{
 
 <template>
     <header>
-        <div class="container-fluid">
-            <img src="../assets/img/dc-logo.png" alt="DC logo">
-            <h1>CIAO</h1>            
-        </div>
+            <div class="row row-cols-2 py-3">
+                <div class="col d-flex justify-content-center align-items-center">
+                    <img src="../assets/img/dc-logo.png" alt="DC logo">
+                </div>
+                <div class="col d-flex align-items-center">
+                    <ul class="d-flex gap-3 fs-5 m-0">
+                        <li v-for="navItems in navLinks">{{ navItems }}</li>
+                    </ul>
+                </div>
+            </div>
     </header>
 </template>
 
 <style lang="scss" scoped>
 @use 'bootstrap/scss/bootstrap.scss' as *;
+img{
+    width: 80px;
+}
 </style>    
