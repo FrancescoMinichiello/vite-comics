b3a900c7c61bf595645679843311e9020067bda0
FrancescoMinichiello <fraps2278@gmail.com>
2024-09-17 16:07:26 +0200

    Added AppMainCardListCurrentSeries and img with the props

---
+++ src/components/AppMainCardListCurrentSeries.vue
@@ -0,0 +1,33 @@
+<script>
+export default{
+  data(){
+    return{
+
+    }
+  },
+  props:{
+    cardThumbs : {
+    type: String,
+    required: true,
+    },
+    cardSeries:{
+        type:String,
+        required: true,
+    }
+  },
+}
+</script>
+
+<template>
+            <div>
+                <img :src="cardThumbs" class="card-thumb" alt="">
+            </div>
+</template>
+
+<style lang="scss" scoped>
+    .card-thumb{
+        height: 150px;
+        width: 150px;
+    }
+
+</style>    
